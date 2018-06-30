b3609709a89ea4963bd72b10d937830e8f1efad0
Siarhei.Baradulia <siarhei_baradulia@epam.com>
2018-06-30 03:59:43 +0300

    Task description

--- Javarush/SumOf10.java
+++ Javarush/SumOf10.java
@@ -1,5 +1,14 @@
 package net.tasks.Javarush;
 
+/*
+The sum of 10 numbers
+Write a program that displays the sum of numbers from 1 to 10 by lines:
+1
+1+2=3
+1+2+3=6
+1+2+3+4=10
+ */
+
 public class SumOf10 {
     public static void main(String[] args) {
         int n = 0;
