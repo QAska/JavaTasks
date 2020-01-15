28a7168475bb9ecf78881ae07d019bed9a06d52e
Sergei Borodulia <sbaradulia@gmail.com>
2020-01-15 18:25:05 +0300

    Thinking Java started

---
+++ ThinkingInJava/Chapter2/HelloDate.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
