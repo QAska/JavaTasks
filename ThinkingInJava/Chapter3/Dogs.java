8d564afcc97c381deaf8a7feff8f60250c6a7a2f
Sergei Borodulia <sbaradulia@gmail.com>
2020-01-21 13:41:29 +0300

    Chapter3, task5 done

--- ThinkingInJava/Chapter3/Dogs.java
+++ ThinkingInJava/Chapter3/Dogs.java
@@ -4,5 +4,21 @@
  * says, "Wurf!". Then display their names and what they say.
  ***********************************************/
 
+class Dog {
+    String name;
+    String says;
+}
 public class Dogs {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+
+        dog1.name = "Spot";
+        dog2.name = "Scruffy";
+        dog1.says = "Ruff!";
+        dog2.says = "Wurf!";
+
+        System.out.println(dog1.name + " says " + dog1.says);
+        System.out.println(dog2.name + " says " + dog2.says);
+    }
 }
