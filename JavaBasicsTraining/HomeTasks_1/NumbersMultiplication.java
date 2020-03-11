9facd5bd424f854fbc36c10d9d13c08172912ed5
Sergei Borodulia <sborodulia@yandex.ru>
2020-03-11 15:54:56 +0300

    Home Task 1, SUM of digits

---
+++ JavaBasicsTraining/HomeTasks_1/NumbersMultiplication.java
@@ -0,0 +1,23 @@
+/*
+Найти произведение цифр заданного четырехзначного числа.
+ */
+
+import java.util.Scanner;
+
+public class NumbersMultiplication {
+    public static void main(String args[]) {
+        int n;
+        int res;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter n:");
+        n = in.nextInt();
+
+        res = 0;
+        while (n != 0) {
+            res = res + n % 10;
+            n = n / 10;
+        }
+        System.out.println("Sum of digits is " + res);
+    }
+}
